1e9bfbe24aede7a34f0a6f135e392dddb84d835e
codehzdler <izalrainpsofficial@gmail.com>
2025-09-09 21:19:43 +0530

    Update supabase.js

--- public/supabase.js
+++ public/supabase.js
@@ -3,7 +3,7 @@
 <script>
   // ⚠️ Replace these with your actual Supabase project values
   const SUPABASE_URL = "https://wsekbfojaqfumpkmzefu.supabase.co";  
-  const SUPABASE_ANON_KEY = "sb_secret_xgJfcM7Sivh4vGszt40aiA_4GpFA7G-";
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzZWtiZm9qYXFmdW1wa216ZWZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyNTE4OTgsImV4cCI6MjA3MjgyNzg5OH0.nmOOPd-1fw-78iIgbWtLUdy5O_g9Q8YxwKNEH1d6TzU";
 
   // Create a global Supabase client
   window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
